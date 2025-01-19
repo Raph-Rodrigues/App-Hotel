c0331e65276be85ba6664dd62ec7769e84a46428
Raphael Rodrigues dos Santos <raphaelrdgss.006@gmail.com>
2025-01-19 16:57:48 -0300

    Creating Project

---
+++ HotelApplication/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelApplication;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ HotelApplication/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace HotelApplication;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
